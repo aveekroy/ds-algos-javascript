7c459de8561b33ab1bda464205818e28bb2e4b0c
Aveek <roy.avk@gmail.com>
2024-06-12 00:57:35 -0700

    binary max heap

---
+++ DATA STRUCTURES/HEAPS/BinaryHeap.js
@@ -0,0 +1,89 @@
+// MaxBinaryHeap (Parent nodes are always larger than the child nodes) - we will implment this here
+// and
+// MinBinaryHeap(Parent nodes are always smaller than the child nodes)
+
+// In Heaps, for any index of array n, the left child is always stored at 2n+1 and right child is always stored at 2n+2
+// For any child at index n, it's parent is at index (n-1)/2 floored
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = []
+  }
+  insert(element) {
+    this.values.push(element)
+    this.bubbleUp()
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1
+    const element = this.values[idx]
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2)
+      let parent = this.values[parentIdx]
+      if (element <= parent) break
+      this.values[parentIdx] = element
+      this.values[idx] = parent
+      idx = parentIdx
+    }
+  }
+
+  removeMax() {
+    const max = this.values[0]
+    const end = this.values.pop()
+    if (this.values.length > 0) {
+      this.values[0] = end
+      this.sinkDown()
+    }
+    return max
+  }
+
+  sinkDown() {
+    let idx = 0
+    const length = this.values.length
+    const element = this.values[0]
+    while (true) {
+      let leftChildIdx = 2 * idx + 1
+      let rightChildIdx = 2 * idx + 2
+      let leftChild, rightChild
+      let swap = null
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx]
+        if (leftChild > element) {
+          swap = leftChildIdx
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx]
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx
+        }
+      }
+
+      if (swap === null) break
+      this.values[idx] = this.values[swap]
+      this.values[swap] = element
+      idx = swap
+    }
+  }
+}
+
+let heap = new MaxBinaryHeap()
+heap.insert(41)
+heap.insert(39)
+heap.insert(33)
+heap.insert(27)
+heap.insert(18)
+heap.insert(55)
+heap.insert(12)
+heap.insert(98)
+
+console.log(heap)
+
+heap.removeMax()
+heap.removeMax()
+heap.removeMax()
+
+console.log(heap)
