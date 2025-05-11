0452d1a9d65c76f92c08b567a14cc095d34cc649
Aveek <roy.avk@gmail.com>
2025-05-11 10:41:05 -0700

    graph problems

--- CODING PLATFORMS/EDUCATIVE.IO/PROBLEM-SOLVING-PATTERNS/DS-PATTERNS/GRAPHS/5_busRoutes.js
+++ CODING PLATFORMS/EDUCATIVE.IO/PROBLEM-SOLVING-PATTERNS/DS-PATTERNS/GRAPHS/5_busRoutes.js
@@ -1,7 +1,7 @@
 // Bus Routes Problem
 
-//  You are given an array, routes, representing bus routes where routes[i] is a bus route that the ith bus repeats forever. Every route contains one or more stations. 
-//  You have also been given the source station, src, and a destination station, dest. 
+//  You are given an array, routes, representing bus routes where routes[i] is a bus route that the ith bus repeats forever. Every route contains one or more stations.
+//  You have also been given the source station, src, and a destination station, dest.
 //  Return the minimum number of buses someone must take to travel from src to dest, or return -1 if there is no route.
 
 // leetcode problem link: https://leetcode.com/problems/bus-routes/
